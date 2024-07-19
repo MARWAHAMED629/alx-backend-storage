94bc17bca3ce86902f297f1f9c34fd3b9689230e
root <mmero385@gmail.com>
2024-07-19 21:38:45 +0000

    update file w

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,38 @@
 #!/usr/bin/env python3
-"""A module with tools for request caching and tracking."""
-
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
+
 redis_store = redis.Redis()
-"""The module-level Redis instance."""
+'''The module-level Redis instance.
+'''
+
 
 def data_cacher(method: Callable) -> Callable:
-    """Caches the output of fetched data."""
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
     def invoker(url) -> str:
-        """The wrapper function for caching the output."""
+        '''The wrapper function for caching the output.
+        '''
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
 
+
 @data_cacher
 def get_page(url: str) -> str:
-    """Returns the content of a URL after caching the request's response,
-    and tracking the request."""
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
     return requests.get(url).text
-
-if __name__ == "__main__":
-    url = "http://google.com"
-    print(get_page(url))  # First call, should fetch and cache
-    print(get_page(url))  # Second call, should return cached result
-
-    # Check the count
-    print(redis_store.get(f'count:{url}'))  # Should print the count of requests
